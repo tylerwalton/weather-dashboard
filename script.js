73e7b14ea049f367ea46fa96859060ebb6305015
Tyler Walton <tylerwalton300@gmail.com>
2022-11-16 22:22:35 -0500

    adding pseudo coding and some JS

---
+++ script.js
@@ -0,0 +1,25 @@
+var button = document.querySelector ('button');
+var name = document.querySelector ('name');
+var description = document.querySelector ('description');
+var tempature = document.querySelector ('tempature');
+var searchHistory = document.querySelector ('history');
+
+fetch ('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=aba22bac1a603ba9277fb78340a99600')
+
+
+// Function to display the search history list.
+function renderSearchHistory() {
+  searchHistory.innerHTML = '';
+}
+
+// Function to show 5 day forecast.
+
+
+// Function to store search history
+
+
+// day.js for current date and time?
+
+
+// Call parameters to present the city name, date, an icon representation of weather conditions,temperature,humidity and wind speed
+
