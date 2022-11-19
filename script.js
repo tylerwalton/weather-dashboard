be6358cac7c306af7e60d288e8d9dc19da6c665d
Tyler Walton <tylerwalton300@gmail.com>
2022-11-18 19:30:32 -0500

    adding click event

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var button = document.querySelector ('button');
-var name = document.querySelector ('name');
-var description = document.querySelector ('description');
-var tempature = document.querySelector ('tempature');
+var button = document.querySelector ('#search-button');
+var searchInput = document.querySelector ('#search-input');
 var searchHistory = document.querySelector ('history');
 
-var weatherApiRootUrl = "https://api.openweathermap.org";
+// var weatherApiRootUrl = "https://api.openweathermap.org";
 var weatherApiKey = "aba22bac1a603ba9277fb78340a99600";
 
 
 // fetch ('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=')
+
+button.addEventListener('click',function(){
+  var cityValue = searchInput.value 
+  console.log(cityValue);
+}) 
+
+function geoCode (searchValue) {
+  fetch (``)
+  .then (response => response.json()) 
+  .then (data => {
+  console.log(data);
+  })
+}
+
 
 
 // Function to display the search history list.
@@ -16,7 +28,7 @@ function renderSearchHistory() {
   searchHistory.innerHTML = '';
 }
 
-// Function to get search history from local storage
+// Function to get search history from local storage.
 function initSearchHistory() {
   var storedHistory = localStorage.getItem('history');
   if (storedHistory) {
@@ -27,7 +39,7 @@ function initSearchHistory() {
 // Function to show 5 day forecast.
 
 
-// Function to store search history
+// Function to store search history.
 
 
 // day.js for current date and time?
