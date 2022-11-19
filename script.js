66a01bd8c3a0cb16913fc09dc8689786bfb6d7bf
Tyler Walton <tylerwalton300@gmail.com>
2022-11-18 21:17:28 -0500

    adding JS functions for current weather and forcast

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 var button = document.querySelector ('#search-button');
 var searchInput = document.querySelector ('#search-input');
 var searchHistory = document.querySelector ('history');
-
-// var weatherApiRootUrl = "https://api.openweathermap.org";
 var weatherApiKey = "aba22bac1a603ba9277fb78340a99600";
-
-
-// fetch ('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=')
 
 button.addEventListener('click',function(){
   var cityValue = searchInput.value 
   console.log(cityValue);
+  geoCode (cityValue)
 }) 
 
 function geoCode (searchValue) {
-  fetch (``)
-  .then (response => response.json()) 
-  .then (data => {
-  console.log(data);
-  })
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&limit=5&appid=aba22bac1a603ba9277fb78340a99600`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      currentWeather (data[0].lat, data[0].lon)
+      forecast(data[0].lat, data[0].lon);
+    });
 }
 
+function currentWeather(lat,lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=aba22bac1a603ba9277fb78340a99600`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
+    var temp = $('<h2>').text('Tempature: '+ data.main.temp )
+    var humidity = document.createElement('h2')
+      humidity.textContent= 'Humidity: ' + data.main.humidity;
+    var wind = $('<h2>').text('Wind: '+ data.wind.speed )
+
+
+
+    $('#current-weather').append(temp, humidity,wind)
+    });
+}
 
 // Function to display the search history list.
 function renderSearchHistory() {
@@ -37,13 +53,25 @@ function initSearchHistory() {
   renderSearchHistory();
 }
 // Function to show 5 day forecast.
+function forecast(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=aba22bac1a603ba9277fb78340a99600`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      for (var i=4; i<data.list.length; i=i+8) {
+        console.log(data.list[i]);
+
+        var forecastTemp = $('<p>').text('Tempature: ' + data.list[i].main.temp) 
+        var forecastHumidity = $('<p>').text('Humidity: ' + data.list[i].main.humidity)
+        var forecastWind = $('<p>').text('Wind: '+ data.list[i].wind.speed)
 
 
+        $('#forecast').append(forecastTemp, forecastHumidity, forecastWind)
+      }
+    });
+}
 // Function to store search history.
 
-
-// day.js for current date and time?
-
-
-// Call parameters to present the city name, date, an icon representation of weather conditions,temperature,humidity and wind speed
 
